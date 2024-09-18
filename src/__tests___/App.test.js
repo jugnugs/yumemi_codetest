ba456d3792d4cdb3d04c903f90debb04626d26ea
Julia Nguyen <julialn.sunni@gmail.com>
2024-09-18 16:10:31 +0900

    add tests for initial mount

---
+++ src/__tests___/App.test.js
@@ -0,0 +1,37 @@
+import { flushPromises, mount } from '@vue/test-utils';
+import { expect, test, vi } from 'vitest';
+import App from '../App.vue';
+import axios from 'axios';
+
+const mockPrefectureData = [
+    {
+        "prefCode": 1,
+        "prefName": "北海道"
+    },
+    {
+        "prefCode": 2,
+        "prefName": "青森県"
+    },
+    {
+        "prefCode": 3,
+        "prefName": "岩手県"
+    },
+    {
+        "prefCode": 4,
+        "prefName": "宮城県"
+    },
+    {
+        "prefCode": 5,
+        "prefName": "秋田県"
+    }
+];
+vi.spyOn(axios, 'get').mockResolvedValue(mockPrefectureData);
+
+test('fetch prefecture data on initial render', async () => {
+    mount(App);
+
+    await flushPromises();
+
+    expect(axios.get).toHaveBeenCalledTimes(1);
+    expect(axios.get).toHaveReturnedWith(mockPrefectureData);
+});
